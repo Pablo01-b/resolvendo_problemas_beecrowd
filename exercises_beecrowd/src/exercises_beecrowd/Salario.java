99654de21b031e284d28ec7164e6e7dc66c874c3
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 19:05:05 -0300

    Calcula o salário do funcionário

---
+++ exercises_beecrowd/src/exercises_beecrowd/Salario.java
@@ -0,0 +1,27 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Salario {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int numeroFuncionario, qtdHoras;
+		double valorHora;
+		
+		numeroFuncionario = sc.nextInt();
+		qtdHoras = sc.nextInt();
+		valorHora = sc.nextDouble();
+		
+		System.out.println("NUMBER = " + numeroFuncionario);
+		System.out.printf("SALARY = U$ %.2f%n", (valorHora * qtdHoras));
+		
+		sc.close();
+
+	}
+
+}
