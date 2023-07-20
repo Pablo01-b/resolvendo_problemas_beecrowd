a9d456b33bdf8a2e83d9359c0412535abdeae83c
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 18:39:11 -0300

    Media entre 3 notas com pesos definidos

---
+++ exercises_beecrowd/src/exercises_beecrowd/media2.java
@@ -0,0 +1,22 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class media2 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C;
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		System.out.printf("MEDIA = %.1f%n", (A * 2 + B * 3 + C * 5) / 10);
+	
+		sc.close();
+	}
+
+}
