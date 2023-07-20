c353fa6083d7961ad22960fd85864a36f2766b35
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 19:40:43 -0300

    BEE 1011

---
+++ exercises_beecrowd/src/exercises_beecrowd/esfera.java
@@ -0,0 +1,20 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class esfera {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double raio = sc.nextDouble();
+		
+		System.out.printf("VOLUME = %.3f%n", (4/3.0) * 3.14159 * Math.pow(raio, 3));
+		
+		sc.close();
+	}
+
+}
