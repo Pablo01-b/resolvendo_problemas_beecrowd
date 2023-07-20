7596c1ad9b4d2554662071d0fcfd0ba415ef9e1d
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 19:54:28 -0300

    BEE 1012

---
+++ exercises_beecrowd/src/exercises_beecrowd/area.java
@@ -0,0 +1,27 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class area {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C;
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		System.out.printf("TRIANGULO: %.3f%n", A * C / 2);
+		System.out.printf("CIRCULO: %.3f%n", 3.14159 * Math.pow(C, 2));
+		System.out.printf("TRAPEZIO: %.3f%n", (A + B) * C / 2);
+		System.out.printf("QUADRADO: %.3f%n", B * B);
+		System.out.printf("RETANGULO: %.3f%n", A * B );
+	
+		sc.close();
+	}
+
+}
