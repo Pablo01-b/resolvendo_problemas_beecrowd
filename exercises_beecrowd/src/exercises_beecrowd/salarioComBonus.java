e610843d81c270663da0cc4eac68484069229ef4
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 19:15:54 -0300

    Calcula o salário do funcionário + comissão de 15% sobre o total vendas

---
+++ exercises_beecrowd/src/exercises_beecrowd/salarioComBonus.java
@@ -0,0 +1,26 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class salarioComBonus {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nomeVendedor;
+		double salarioFixo, montanteVendas;
+		
+		nomeVendedor = sc.nextLine();
+		salarioFixo = sc.nextDouble();
+		montanteVendas = sc.nextDouble();
+		
+		System.out.printf("TOTAL = R$ %.2f%n", (montanteVendas * 0.15 + salarioFixo));
+		
+		
+		sc.close();
+	}
+
+}
