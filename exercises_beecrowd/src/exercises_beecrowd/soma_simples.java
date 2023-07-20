c43de0aef90a6bb58f9682d969639f7e91209897
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 15:21:51 -0300

    Exercício 02

---
+++ exercises_beecrowd/src/exercises_beecrowd/soma_simples.java
@@ -0,0 +1,20 @@
+package exercises_beecrowd;
+
+import java.util.Scanner;
+
+public class soma_simples {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, SOMA;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		SOMA = A + B;
+		
+		System.out.println("SOMA = " + SOMA);
+		
+		sc.close();
+	}
+}
