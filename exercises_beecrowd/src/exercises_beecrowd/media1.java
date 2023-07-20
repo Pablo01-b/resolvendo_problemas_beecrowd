a00e63c45425fc96aa680707f38f2d90d95bc86b
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 18:29:49 -0300

    Media entre 2 valores com pesos pre-definidos

---
+++ exercises_beecrowd/src/exercises_beecrowd/media1.java
@@ -0,0 +1,23 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class media1 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B;
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		
+		System.out.printf("MEDIA = %.5f%n", (A * 3.5 + B * 7.5) / 11);
+		
+		sc.close();
+
+	}
+
+}
