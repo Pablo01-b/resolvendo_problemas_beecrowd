efbb97652925e8a7d4ee865dcc489b988d11e421
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 18:49:15 -0300

    calculo de diferenca considerando a formula: (A* B - C * D)

---
+++ exercises_beecrowd/src/exercises_beecrowd/diferenca.java
@@ -0,0 +1,25 @@
+package exercises_beecrowd;
+
+import java.util.Scanner;
+
+public class diferenca {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C, D;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		
+		double diferenca = (A* B - C * D);
+		
+		System.out.printf("DIFERENCA = %.0f%n", diferenca);
+		
+		sc.close();
+
+	}
+
+}
