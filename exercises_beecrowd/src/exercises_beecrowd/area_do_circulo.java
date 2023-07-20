e48cc2ca5b7e8317e2dac88290fe22544b45d9a4
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-20 15:12:01 -0300

    Exercício 01

---
+++ exercises_beecrowd/src/exercises_beecrowd/area_do_circulo.java
@@ -0,0 +1,18 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class area_do_circulo {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double raio = sc.nextDouble();
+		System.out.printf("A=%.4f%n", (3.14159 * Math.pow(raio, 2)));
+
+		sc.close();
+	}
+}
