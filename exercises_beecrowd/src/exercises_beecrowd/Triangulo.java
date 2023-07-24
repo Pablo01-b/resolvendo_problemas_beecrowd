cb28717b93fdc153c3afa1c34d04a90ab6fd41f7
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-24 18:18:17 -0300

    BEE 1043

---
+++ exercises_beecrowd/src/exercises_beecrowd/Triangulo.java
@@ -0,0 +1,29 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C, P;
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		if(A + B > C && A + C > B && B + C > A) {
+			P = A + B + C;
+			System.out.printf("Perimetro = %.1f%n", P);
+		}else {
+			P = ((A + B) * C ) / 2;
+			System.out.printf("Area = %.1f%n", P);
+		}
+		
+		sc.close();
+		
+	}
+}
