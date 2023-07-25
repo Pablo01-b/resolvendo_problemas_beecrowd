e4e6f0e460d76b65d64c28d39fba8209c794ca82
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-25 18:21:38 -0300

    BEE 1046

---
+++ exercises_beecrowd/src/exercises_beecrowd/TempoJogo.java
@@ -0,0 +1,29 @@
+package exercises_beecrowd;
+
+import java.util.Scanner;
+
+public class TempoJogo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int horaInicial, horaFinal, duracao;
+		horaInicial = sc.nextInt();
+		horaFinal = sc.nextInt();
+		
+		if(horaInicial == 0 && horaFinal == 0) {
+			duracao = 24;
+			System.out.printf("O JOGO DUROU %d HORA(S)%n", duracao);
+		}else if(horaInicial < horaFinal) {
+			duracao = (horaFinal - horaInicial);
+			System.out.printf("O JOGO DUROU %d HORA(S)%n", duracao);
+		}else {
+			duracao = (horaInicial - horaFinal - 24) * (-1);
+			System.out.printf("O JOGO DUROU %d HORA(S)%n", duracao);
+		}
+		
+		sc.close();
+	}
+
+}
