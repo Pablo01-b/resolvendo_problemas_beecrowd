e3c3d16ba8a79447ae69a56ec73be3741dc0b04a
Pablo Bruci <pablobrucipb@gmail.com>
2023-07-30 19:58:39 -0300

    BEE 1048

---
+++ exercises_beecrowd/src/exercises_beecrowd/AumentoSalario.java
@@ -0,0 +1,61 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AumentoSalario {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double salario = sc.nextDouble();
+		double porcentagem = 0;
+		
+		if(salario > 0 && salario <= 400.00) {
+			porcentagem += 0.15;
+			porcentagem = salario * porcentagem;
+			salario += porcentagem;
+			System.out.printf("Novo salario: %.2f%n", salario);
+			System.out.printf("Reajuste ganho: %.2f%n", porcentagem);
+			System.out.println("Em percentual: " + "15 %");
+			
+		}else if (salario > 400.00 && salario <= 800.00) {
+			porcentagem += 0.12;
+			porcentagem = salario * porcentagem;
+			salario += porcentagem;
+			System.out.printf("Novo salario: %.2f%n", salario);
+			System.out.printf("Reajuste ganho: %.2f%n", porcentagem);
+			System.out.println("Em percentual: " + "12 %");
+			
+		}else if (salario > 800.00 && salario <= 1200.00) {
+			porcentagem += 0.10;
+			porcentagem = salario * porcentagem;
+			salario += porcentagem;
+			System.out.printf("Novo salario: %.2f%n", salario);
+			System.out.printf("Reajuste ganho: %.2f%n", porcentagem);
+			System.out.println("Em percentual: " + "10 %");
+			
+		}else if (salario > 1200.00 && salario <= 2000.00) {
+			porcentagem += 0.07;
+			porcentagem = salario * porcentagem;
+			salario += porcentagem;
+			System.out.printf("Novo salario: %.2f%n", salario);
+			System.out.printf("Reajuste ganho: %.2f%n", porcentagem);
+			System.out.println("Em percentual: " + "7 %");
+			
+		}else if(salario > 2000.00) {
+			porcentagem += 0.04;
+			porcentagem = salario * porcentagem;
+			salario += porcentagem;
+			System.out.printf("Novo salario: %.2f%n", salario);
+			System.out.printf("Reajuste ganho: %.2f%n", porcentagem);
+			System.out.println("Em percentual: " + "4 %");
+		}
+	
+		sc.close();
+
+	}
+
+}
