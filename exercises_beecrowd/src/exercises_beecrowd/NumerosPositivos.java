68c352c7362c6815c68fdb49e499d336cc885b12
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-07 20:50:33 -0300

    BEE 1060

---
+++ exercises_beecrowd/src/exercises_beecrowd/NumerosPositivos.java
@@ -0,0 +1,28 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NumerosPositivos {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int qtdPositivos = 0;
+		
+		for(int i = 1; i <= 6; i++) {
+			double valor = sc.nextDouble();
+			if(valor > 0) {
+				qtdPositivos++;
+			}
+		}
+		
+		System.out.println(qtdPositivos + " valores positivos");
+		
+		sc.close();
+
+	}
+
+}
