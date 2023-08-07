6950ec2e3d91b4f947060595d4666c69a0782760
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-07 20:36:43 -0300

    BEE 1059

---
+++ exercises_beecrowd/src/exercises_beecrowd/NumerosPares.java
@@ -0,0 +1,16 @@
+package exercises_beecrowd;
+
+public class NumerosPares {
+
+	public static void main(String[] args) {
+		
+		/*Esse problema é extremamente simples*/
+		for(int i = 1; i<= 100; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
