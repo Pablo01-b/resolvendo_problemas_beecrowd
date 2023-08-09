f55b60b4d7e2a69ccf54bae0de6db163924da172
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-09 19:40:04 -0300

    PROBLEMA: 1066 - Pares, Ímpares, Positivos e Negativos

---
+++ exercises_beecrowd/src/exercises_beecrowd/ParesImparesPositivosNegativos.java
@@ -0,0 +1,39 @@
+package exercises_beecrowd;
+
+import java.util.Scanner;
+
+public class ParesImparesPositivosNegativos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numeros, contadorPares, contadorImpares, contadorPositivos, contadorNegativos;
+		contadorPares = 0;
+		contadorImpares = 0;
+		contadorPositivos = 0;
+		contadorNegativos = 0;
+		
+		for(int i = 1; i <= 5; i++) {
+			numeros = sc.nextInt();
+			if(numeros % 2 == 0) {
+				contadorPares++;
+			}if(numeros % 2 != 0) {
+				contadorImpares++;
+			}if(numeros > 0) {
+				contadorPositivos++;
+			}if(numeros < 0) {
+				contadorNegativos++;
+			}
+		}
+		
+		System.out.println(contadorPares + " valor(es) par(es)");
+		System.out.println(contadorImpares + " valor(es) impar(es)");
+		System.out.println(contadorPositivos + " valor(es) positivo(s)");
+		System.out.println(contadorNegativos + " valor(es) negativo(s)");
+		
+		sc.close();
+
+	}
+
+}
