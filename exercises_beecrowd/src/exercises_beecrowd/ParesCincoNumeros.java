1c77b222e740733b8e39194ca6c0b150f90f71d0
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-09 19:42:19 -0300

    PROBLEMA: 1065 - Pares entre Cinco Números

---
+++ exercises_beecrowd/src/exercises_beecrowd/ParesCincoNumeros.java
@@ -0,0 +1,26 @@
+package exercises_beecrowd;
+
+import java.util.Scanner;
+
+public class ParesCincoNumeros {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numeros;
+		int contador = 0;
+		
+		for(int i = 1; i <= 5; i++) {
+			numeros = sc.nextInt();
+			if(numeros % 2 == 0) {
+				contador++;
+			}
+		}
+		
+		System.out.println(contador + " valores pares");
+		
+		sc.close();
+	}
+
+}
