e7c55f33c8df2337dec8375fd476181d9f62c75e
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-09 19:19:35 -0300

    PROBLEMA: 1064 - Positivos e Média

---
+++ exercises_beecrowd/src/exercises_beecrowd/PositivosMedia.java
@@ -0,0 +1,34 @@
+package exercises_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class PositivosMedia {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int contador = 0;
+		double numeros = 0;
+		double media = 0;
+		double positivos = 0;
+		
+		for(int i = 1; i <= 6; i++) {
+			numeros = sc.nextDouble();
+			if(numeros > 0) {
+				contador++;
+				positivos += numeros;
+				media = positivos / contador;
+			}
+		}
+		
+		System.out.println(contador + " valores positivos");
+		System.out.printf("%.1f%n", media);
+		
+		sc.close();
+
+	}
+
+}
