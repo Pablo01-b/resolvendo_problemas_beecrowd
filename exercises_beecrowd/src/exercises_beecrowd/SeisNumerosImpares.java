2d02695778457985c70360fbb317eb1d5c07a16a
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-10 18:31:28 -0300

    PROBLEMA:1070 - Seis Números Ímpares

---
+++ exercises_beecrowd/src/exercises_beecrowd/SeisNumerosImpares.java
@@ -0,0 +1,23 @@
+package exercises_beecrowd;
+
+import java.util.Scanner;
+
+public class SeisNumerosImpares {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=6; n++) {
+			if(n % 2 != 0) {
+				System.out.println(n);
+				i++;
+			}
+		}
+		
+		sc.close();
+	}
+
+}
