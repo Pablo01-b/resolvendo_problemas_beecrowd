f4a667170fa7a6c95ef3a0adb7b54df4e0444ebf
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-10 18:20:42 -0300

    PROBLEMA:1067 - Números Ímpares

--- exercises_beecrowd/src/exercises_beecrowd/NumerosImpares.java
+++ exercises_beecrowd/src/exercises_beecrowd/NumerosImpares.java
@@ -6,15 +6,15 @@ public class NumerosImpares {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		
-		for(int i=1; i<=n; i++) {
-			if(i % 2 != 0) {
+
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 != 0) {
 				System.out.println(i);
 			}
 		}
-			
+
 		sc.close();
 
 	}
