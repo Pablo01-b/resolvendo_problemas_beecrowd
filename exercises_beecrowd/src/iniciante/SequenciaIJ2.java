6464c4774fbafef78a8c2bfb0d6e667d58635a89
Pablo Bruci <pablobrucipb@gmail.com>
2023-08-16 18:31:13 -0300

    BEE 1096

---
+++ exercises_beecrowd/src/iniciante/SequenciaIJ2.java
@@ -0,0 +1,16 @@
+package iniciante;
+
+public class SequenciaIJ2 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 9; i += 2) {
+            for (int j = 7; j >= 5; j--) {
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+
+		//BEE 1096
+	}
+
+}
